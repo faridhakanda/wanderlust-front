4b5b653f5938feb49565c91457dcd0b7c6dc2d6b
faridhakanda <faridakanda1729@gmail.com>
2026-05-17 09:42:35 +0600

    error page added

---
+++ src/app/error.js
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h2>Main error page</h2>
+        </div>
+    );
+};
+
+export default ErrorPage;
